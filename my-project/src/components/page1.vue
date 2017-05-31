6f9e39acd459b15ae9d233f38668874662dcf4c3
zzz <865077695@qq.com>
2017-05-31 18:57:11 +0800

    修正bug

---
+++ my-project/src/components/page1.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="page1">
+    asd
+    <h1>{{ msg }}</h1>
+  
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'page1',
+    data () {
+      return {
+        msg: '页面1'
+      }
+    }
+  }
+
+</script>
+
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+  
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  
+  a {
+    color: #42b983;
+  }
+</style>
--- my-project/src/router/index.js
+++ my-project/src/router/index.js
@@ -3,16 +3,23 @@ import Vue from 'vue'
 // 引入vue-router
 import Router from 'vue-router'
 // 引入Hello组件
-import Hello from '@/components/Hello'//
+import Hello from '@/components/Hello'
+// 引入page1组件
+import page1 from '@/components/page1'
 
 Vue.use(Router)
 
 const router = new Router({
   routes: [
     {
-      path: '/',
+      path: '/Hello',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/page1',
+      name: 'page1',
+      component: page1
     }
   ]
 })
